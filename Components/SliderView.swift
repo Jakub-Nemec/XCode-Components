49284196d673c3cf508b221a10f65ee3ef0d3ef8
Jakub Němec <Jakub.K.Nmec@gmail.com>
2020-07-30 14:51:01 +0200

    Without class

---
+++ Components/SliderView.swift
@@ -0,0 +1,21 @@
+//
+//  SliderView.swift
+//  Components
+//
+//  Created by Jakub Němec on 23/07/2020.
+//  Copyright © 2020 Jakub Němec. All rights reserved.
+//
+
+import UIKit
+
+class SliderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Components/TFView.swift
@@ -0,0 +1,21 @@
+//
+//  TFView.swift
+//  Components
+//
+//  Created by Jakub Němec on 23/07/2020.
+//  Copyright © 2020 Jakub Němec. All rights reserved.
+//
+
+import UIKit
+
+class TFView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
